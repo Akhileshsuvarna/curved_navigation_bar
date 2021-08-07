2041281a66db5203e2bf5a49f848057f53e759a2
Akhilesh Suvarna <52662412+Akhileshsuvarna@users.noreply.github.com>
2021-08-08 02:08:34 +0530

    Update curved_navigation_bar.dart

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -143,12 +143,12 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
  Container(
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(24),
-          topRight: Radius.circular(24),
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
               border: Border.all(
-                            color: Colors.red,
-                            width: 5),
+                           color: Color(0xFFEBECEE),
+                            width: 1),
       ),
       child: Positioned(
         left: 0,
